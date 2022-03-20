6b357dea19d338fe07b218b051bbd80778ce52d2
Megan Gedde <hoglu070@d.umn.edu>
2022-03-20 18:03:04 -0500

    added readme data

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,13 @@
 // Assignment code here
 
-var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "["];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "["];
 
 function generatePassword() {
   var password = [];
-  var selectedChar = [];
+  var selectedCriteria = [];
   var passwordLength = parseInt(prompt("How long would you like your password to be? Enter a number 8-128."));
   //Validates that the users desired password length is between 8-128.
   if (passwordLength < 8 || passwordLength > 128) {
@@ -29,19 +29,19 @@ function generatePassword() {
     window.alert("You must select 'OK' to at least one character type.");
     return generatePassword();
   }
-  //Adds chosen character types into new array called selectedChar.
-  if (includeLowercase) selectedChar = selectedChar.concat(lowerCaseChar);
+  //Adds chosen character types into new array called selectedCriteria.
+  if (includeLowercase) selectedCriteria = selectedCriteria.concat(lowerCase);
 
-  if (includeUppercase) selectedChar = selectedChar.concat(upperCaseChar);
+  if (includeUppercase) selectedCriteria = selectedCriteria.concat(upperCase);
 
-  if (includeNumbers) selectedChar = selectedChar.concat(numberChar);
+  if (includeNumbers) selectedCriteria = selectedCriteria.concat(number);
 
-  if (includeSpecial) selectedChar = selectedChar.concat(specialChar);
+  if (includeSpecial) selectedCriteria = selectedCriteria.concat(special);
 
   //this loop will choose a random character in the array of possible characters and adds one character in each iteration to a password array until it meets the users desired password length.
   for (var i = 0; i < passwordLength; i++) {
-    var randomArrayPosition = Math.floor(Math.random() * selectedChar.length);
-    var eachCharacter = selectedChar[randomArrayPosition];
+    var randomArrayPosition = Math.floor(Math.random() * selectedCriteria.length);
+    var eachCharacter = selectedCriteria[randomArrayPosition];
     password.push(eachCharacter);
   }
 
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Password Generator Starter Code
+
+## Description
+This is a password generator that assists in making secure passwords for sensitive date. It will randomly generate a password that meets certain criteria in order to create a strong password that provides greater security.
+
+## Webpage
+The webpage will present the user with a series of prompts for password criteria including length of password, and will require at least one of the following: lowercase, uppercase, numberic, and/or special characters.
+
+Once all prompts are correctly answered, you will receive a password like in this screenshot.
+
+![website screenshot](develop/website.jpg)
+
+
+## Built With
+This website uses the following:
+* HTML
+* CSS
+* Javascript
+
+
+Visit the live site here: https://meggedde.github.io/Challenge3/
